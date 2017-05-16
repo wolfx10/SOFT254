303032ebd997165e89070e22a4b7775109d285b8
(s) Neil Cooke <ncooke@pm7-0662.local>
2017-05-16 17:59:40 +0100

    Initial Commit

---
+++ Assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by (s) Neil Cooke on 16/05/2017.
+//  Copyright © 2017 (s) Neil Cooke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
